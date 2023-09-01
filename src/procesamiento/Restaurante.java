017d985f2afa3827050f7f0d806e1c94799f06a1
Carlos <c.penaa@uniandes.edu.co>
2023-08-31 20:00:06 -0500

    a1.12

--- src/procesamiento/Restaurante.java
+++ src/procesamiento/Restaurante.java
@@ -2,7 +2,6 @@ package procesamiento;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
