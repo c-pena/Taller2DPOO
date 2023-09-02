f96266ee597b8e4fbff5e8941ebf3b367db8560b
Carlos <c.penaa@uniandes.edu.co>
2023-09-02 11:21:24 -0500

    a6.1

--- src/consola/Aplicacion.java
+++ src/consola/Aplicacion.java
@@ -4,8 +4,6 @@ import procesamiento.Restaurante;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Paths;
-import java.io.File;
 
 public class Aplicacion {
 	
--- src/procesamiento/Restaurante.java
+++ src/procesamiento/Restaurante.java
@@ -3,7 +3,6 @@ package procesamiento;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
